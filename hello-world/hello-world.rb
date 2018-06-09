3d08b53fa39c303825b5933595ac75dd2990e19b
Thiago Galvani <thiagopaiva99@gmail.com>
2018-06-09 17:51:54 -0300

    Hello world and variable manipulation

---
+++ hello-world/hello-world.rb
@@ -0,0 +1,16 @@
+# Printing some things on console
+puts 'Hello World!'
+puts 5 + 3
+
+# Putting value on variables
+name = 'Thiago'
+color = 'Teal'
+number = 10
+
+# Concating variables
+puts 'Hello  ' + name + '. Welcome to the Ruby world!'
+puts name + 'test' # Thiagotest
+puts "#{name} test" # Thiago teste
+
+# Variable interpolation
+puts "Hello #{name}. Welcome to the Ruby world!"
